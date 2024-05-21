e20126aa2eff1b980c133283aa3233f65e6f5ed0
Devon DeRaad <devonderaad@Devons-MBP.attlocal.net>
2024-05-20 19:21:44 -0500

    Add verify snapp results folder to respond to reviewers

---
+++ snapp/verify.snapp.results/rep2.r2/run.snapp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+#SBATCH --job-name=snapp               # Job Name
+#SBATCH --nodes=1             # 40 nodes
+#SBATCH --ntasks-per-node=25               # 40 CPU allocation per Task
+#SBATCH --partition=bi            # Name of the Slurm partition used
+#SBATCH --chdir=/home/d669d153/work/nmel.ceyx/snapp/rep2.r2    # Set working d$
+#SBATCH --mem-per-cpu=800            # memory requested
+#SBATCH --time=10000
+
+#run beast 2.7.1
+/home/d669d153/work/beast.2.7.1/beast/bin/beast -threads 25 -seed 775 rep2.xml
+
